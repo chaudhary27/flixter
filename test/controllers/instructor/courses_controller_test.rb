a50890a7a4d79d1de7728289cae4456504f4b066
Faisal <chaudhary.fsl@gmail.com>
2015-08-07 19:48:07 +0000

    failed to add test course_added

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "course_added" do
+     user = FactoryGirl.create(:user)
+     sign_in user
+
+     	course = FactoryGirl.create(:course)
+
+     		assert_difference 'Course.count' do
+     			post :create, :title => course.title, :description => course.description, :cost => course.cost
+     		end
+     		assert_equal 1, course.count
+     		assert_redirected_to instructor_course_path(course)
+   end
+
 end
--- test/factories.rb
+++ test/factories.rb
@@ -1,7 +1,17 @@
 FactoryGirl.define do
+
 	factory :course do
 		title "Java for Beginners"
-		description "Best couse for Beginners"
+		description "Best course for Beginners"
 		cost "100"
 	end
+
+	factory :user do
+		sequence :email do |n|
+			"testemail#{n}@gmail.com"
+		end
+		password "flixterbaby"
+		password_confirmation "flixterbaby"
+	end
+
 end
