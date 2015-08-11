b1db22a0e074d606b5a290cf5aaf3be300abd2a6
Faisal <chaudhary.fsl@gmail.com>
2015-08-11 01:22:46 +0000

    require users to be enrolled in a course to view lesson. Added customized authentication in lessons_controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,13 +1,22 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course
+
 	def show
-		
 	end
 
 	private
 
 	helper_method :current_lesson
-
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def require_authorized_for_current_course
+		@course = current_lesson.section.course
+		if current_user.enrolled_in?(@course) == false
+			redirect_to course_path(@course), :alert => 'Enrollment is required to access lessons'
+		end
+
 	end
 end
